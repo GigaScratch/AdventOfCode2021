fd3e25523ce355141281bd5302a6619255361d92
Olivia <olivia@cuttlefish.fish>
2021-12-05 16:36:49 +1300

    Finished Day 2 puzzle

--- Day2/main.rb
+++ Day2/main.rb
@@ -1,7 +1,7 @@
 
-
 pos = 0
 depth = 0
+
 File.foreach("Day2/input.txt").each do |line|
     splitInput = line.split
     
@@ -9,11 +9,12 @@ File.foreach("Day2/input.txt").each do |line|
     when "forward"
         pos += splitInput[1].to_i
     when "up"
-        depth += splitInput[1].to_i
-    when "down"
         depth -= splitInput[1].to_i
+    when "down"
+        depth += splitInput[1].to_i
     end
 end
 
-puts "Position: ".concat(pos)
-puts "Depth: ".concat(depth)
+puts "Position: ".concat(pos.to_s)
+puts "Depth: ".concat(depth.to_s)
+puts "Puzzle Result: ".concat((pos * depth).to_s)
